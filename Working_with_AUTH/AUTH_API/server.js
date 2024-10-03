20b0c8234437de606a195ce04adb05296cfdaa78
Pranshu_batham <24f1000794@ds.study.iitm.ac.in>
2024-10-03 08:28:46 +0530

    added code

--- Working_with_AUTH/AUTH_API/server.js
+++ Working_with_AUTH/AUTH_API/server.js
@@ -22,3 +22,9 @@ app.get('/api/protected', protect, (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+    console.error('Unhandled rejection:', err.message);
+    process.exit(1);
+});
