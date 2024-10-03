32eef0cbf129ff0f69e5e00eaad331cf16a7f5ad
Pranshu_batham <24f1000794@ds.study.iitm.ac.in>
2024-10-03 08:23:42 +0530

    added code

--- Working_with_AUTH/AUTH_API/controllers/authControllers.js
+++ Working_with_AUTH/AUTH_API/controllers/authControllers.js
@@ -1,54 +1,55 @@
 const User = require('../models/user');
 const generateToken = require('../utils/generateToken');
 
-//Register a new user
-
+// Register a new user
 const register = async (req, res) => {
-    const { email, password } = req.body;
+    const { name, email, password } = req.body;
 
-    // check if user is already avaliable or not
-    const userExists = await User.findOne({ email }); // User mail is unique which I already defined in the schemas.
-    if (userExists) {
-        return res.status(400).json({ message: 'User is already exists' });
-    } 
-    else {
+    try {
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+
         // Create new user
-
         const user = await User.create({ name, email, password });
         if (user) {
             res.status(200).json({
                 _id: user._id,
                 email: user.email,
-                token: generateToken(user._id)
+                token: generateToken(user._id),
             });
         } else {
             res.status(400).json({ message: 'Invalid user data' });
         }
+    } catch (error) {
+        console.error(error); // Log the error
+        res.status(500).json({ message: 'Server error' });
     }
-
-
 };
 
-
+// Authenticate user (login)
 const authUser = async (req, res) => {
     const { email, password } = req.body;
 
-    //check if user is exists or not
-
-    const user = await user.findOne({ email });
-    if (user && (await user.matchPassword(password))) {
-        res.json({
-            _id: user._id,
-            email: user.email,
-            token: generateToken(user._id)
-        });
-    } else {
-        res.status(401).json({ message: 'Invalid email or password' });
+    try {
+        const user = await User.findOne({ email });
+        if (user && (await user.matchPassword(password))) {
+            res.json({
+                _id: user._id,
+                email: user.email,
+                token: generateToken(user._id),
+            });
+        } else {
+            res.status(401).json({ message: 'Invalid email or password' });
+        }
+    } catch (error) {
+        console.error(error); // Log the error
+        res.status(500).json({ message: 'Server error' });
     }
 };
 
-
 module.exports = {
     register,
-    authUser
+    authUser,
 };
