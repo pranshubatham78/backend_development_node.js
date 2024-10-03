5f5b9843e517c1c3cf3ddc3bbfb64a133189d8bb
Pranshu_batham <24f1000794@ds.study.iitm.ac.in>
2024-10-03 08:27:45 +0530

    added code

--- Working_with_AUTH/AUTH_API/utils/generateToken.js
+++ Working_with_AUTH/AUTH_API/utils/generateToken.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (id) => {    // id : we get this from the user while creating their account or login in their account. 
-    jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "500s" })};
+   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "500s" })};
+
+module.exports = generateToken;
