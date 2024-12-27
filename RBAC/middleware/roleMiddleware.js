78bedb426bb7e9795f8a4c29015f44c9e1974c93
pranshubatham78 <24f1000794@ds.study.iitm.ac.in>
2024-12-27 20:41:59 +0530

    Add files via upload

---
+++ RBAC/middleware/roleMiddleware.js
@@ -0,0 +1,14 @@
+// It provide the authorization based on the roles.
+
+const authorizedRoles = (...allowedRoles)=>{
+    return (req,res,next)=>{
+        if(!allowedRoles.includes(req.user.role)){
+            return res.status(403).json({message : "Access denied"});
+        };
+        next();
+    };
+
+};
+
+
+module.exports = authorizedRoles;
