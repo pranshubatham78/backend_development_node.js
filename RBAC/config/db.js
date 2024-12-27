6414ce0513d7fb7a71d5f72b2138a599c0348e8d
pranshubatham78 <24f1000794@ds.study.iitm.ac.in>
2024-12-27 20:36:39 +0530

    Add files via upload

--- RBAC/config/db.js
+++ RBAC/config/db.js
@@ -1,1 +1,37 @@
 
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// function for connecting with the database.
+const connect = async () => {
+    try {
+        const connection = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`Database is connected : ${connection.connection.host}, ${connection.connection.name}`);
+    } catch (error) {
+        console.log('Connection failed');
+        console.log(error);
+        process.exit(1); // Exit the program if any issue arises.
+    };
+};
+
+module.exports = connect;
+=======
+// Connection with mongoose with database
+
+const mongoose = require('mongoose');
+const connectDB = async ()=>{
+    try{
+        const connection = await mongoose.connect("mongodb://localhost:27017/sales",{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000
+        });
+        console.log("Connection sucessfully done");
+ 
+    } catch (error) {
+        console.error("OPPS!, There's something went wrong :(");
+    }
+};
+
+module.exports = connectDB;
+
